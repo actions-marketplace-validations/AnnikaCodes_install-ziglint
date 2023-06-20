29ec3ed37c32bb07ee7ca556901b64de3ba1eb89
Annika <annika@worldbrightening.net>
2023-06-20 13:19:29 -0700

    Be compatible with Node 16

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 const path = require('path');
 const fs = require('fs');
+const https = require('https');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
 const API_URL = 'https://api.github.com/repos/AnnikaCodes/ziglint/releases/latest';
-
-async function writeFilePromise(file, data) {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(file, data, (err) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve();
-            }
-        });
-    });
-}
 
 (async () => {
     try {
@@ -33,8 +22,22 @@ async function writeFilePromise(file, data) {
         if (os === 'windows') name += '.exe';
         core.info(`Looking for ${name}...`);
 
-        const apiRequest = await fetch(API_URL);
-        const latestRelease = await apiRequest.json();
+        // make a JSON request to the GitHub API
+        const latestRelease = await new Promise((resolve, reject) => {
+            https.get(
+                API_URL,
+                {headers: {'User-Agent': 'install-ziglint GitHub Action'}},
+                (response) => {
+                    let data = '';
+                    response.on('data', (chunk) => {
+                        data += chunk;
+                    });
+                    response.on('end', () => {
+                        resolve(JSON.parse(data));
+                    });
+                },
+            ).on('error', (e) => reject(e));
+        });
         core.info(`Latest release is ${latestRelease.name}`);
         const asset = latestRelease.assets.find(asset => asset.name === name);
         if (!asset) {
@@ -44,15 +47,21 @@ async function writeFilePromise(file, data) {
 
         const downloadUrl = asset.browser_download_url;
         core.info(`Downloading ${downloadUrl}...`);
-        const binaryRequest = await fetch(downloadUrl);
-
-        await writeFilePromise(binaryName, Buffer.from(await binaryRequest.arrayBuffer()));
+        const writeStream = fs.createWriteStream(binaryName);
+        await new Promise((resolve, reject) => {
+            https.get(downloadUrl, (response) => {
+                response.pipe(writeStream);
+                writeStream.on('finish', () => {
+                    writeStream.close();
+                    resolve();
+                });
+            }).on('error', (e) => reject(e));
+        });
         core.info(`Successfully downloaded ${binaryName}`);
 
         // make it executable
         if (os !== 'windows') fs.chmodSync(binaryName, 0o755);
         core.addPath(path.resolve(process.cwd(), binaryName));
-
     } catch (error) {
         core.setFailed(error.message);
     }
